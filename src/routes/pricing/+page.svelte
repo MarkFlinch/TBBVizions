62da74affbeeb896f60f2914f7092dc06bcc1f61
Mark Flinch <Flinchmark1@gmail.com>
2023-06-20 15:01:48 -0400

    change img routes

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -1,11 +1,11 @@
 <script>
-  import create from "$lib/imgs/creativevizion.png"
+  import create from "$lib/images/creativevizion.png"
 	import Navbar from "$lib/components/Navbar.svelte";
 	import Hero from "$lib/components/Hero.svelte";
-  import phone from "$lib/imgs/cash_phone.png"
+  import phone from "$lib/images/cash_phone.png"
 	import PricingNav from "../../lib/components/Pricing_Nav.svelte";
 	import Pricing from "../../lib/components/Pricing.svelte";
-  import test from "$lib/imgs/test.png"
+  import test from "$lib/images/test.png"
 </script>
 
 
